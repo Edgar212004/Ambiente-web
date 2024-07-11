cbb1746b86ed520fe377596029a0479ec70fb87c
Edgar <edariasb5@gmail.com>
2024-07-10 20:33:25 -0600

    Node server

---
+++ Server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/",(req, res) => {
+    console.log("Loading home...");
+    res.send("Hello world");
+});
+
+app.listen(PORT, () => (
+    console.log("🎶Server is running on port " + PORT)
+));
+

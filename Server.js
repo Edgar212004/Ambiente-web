6ef24bee952183fee5b73f2554dcbd3fcfcca877
Edgar Arias <edariasb5@gmail.com>
2024-07-11 12:12:03 -0600

    Navegation ready

--- Server.js
+++ Server.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const path = require("path");
 
+
 const app = express();
 const PORT = 3000;
+const   PUBLIC = path.join(__dirname, "public")
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(PUBLIC));
 
 app.get("/",(req, res) => {
     console.log("Loading home...");
-    res.send("Hello world");
+    res.sendFile(path.join(PUBLIC, "home.html"));
 });
+
+app.get("/contactus", (req, res)=>{
+    console.log("loanding contact us...");
+    res.sendFile(path.join(PUBLIC, "Contactus.html"));
+})
+
+app.post("/contactus", (req, res)=>{
+    console.info("contact Us has been called...")
+    const name = req.body.name;
+    const email = req.body.email;
+    const Subject = req.body.Subject;
+
+    console.log("Form data\n");
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Subject: " + Subject);
+
+    res.redirect("/");
+})
 
 app.listen(PORT, () => (
     console.log("🎶Server is running on port " + PORT)
